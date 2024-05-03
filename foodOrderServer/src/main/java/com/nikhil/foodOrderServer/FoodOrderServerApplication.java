e97f295284cdc94e9a465f3e9b34bd46299c24c0
dommarajunikhil26 <92212838+dommarajunikhil26@users.noreply.github.com>
2024-05-03 10:54:23 -0500

    Initial Server Commit

---
+++ foodOrderServer/src/main/java/com/nikhil/foodOrderServer/FoodOrderServerApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.foodOrderServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderServerApplication.class, args);
+	}
+
+}
---
+++ foodOrderServer/src/test/java/com/nikhil/foodOrderServer/FoodOrderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.foodOrderServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
